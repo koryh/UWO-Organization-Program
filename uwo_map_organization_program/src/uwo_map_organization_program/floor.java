2bb720ac7e114882e4512fba2426d4de6a954f04
koryh <henryhan1997@gmail.com>
2019-11-21 17:28:05 -0500

    q

---
+++ uwo_map_organization_program/src/uwo_map_organization_program/floor.java
@@ -0,0 +1,11 @@
+package uwo_map_organization_program;
+
+public class floor {
+
+	private int[] room_numbers;
+	
+	public floor (int[] room_numbers) {
+		this.room_numbers = room_numbers;
+	}
+	
+}
